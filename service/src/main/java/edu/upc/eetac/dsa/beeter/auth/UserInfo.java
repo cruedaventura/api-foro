ce80db9de3330ff6b3762cef2e0a7128c54ccffa
Carolina Rueda <lina_bcn@hotmail.com>
2015-10-04 16:26:33 +0200

    DAO added

---
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/auth/UserInfo.java
@@ -0,0 +1,35 @@
+package edu.upc.eetac.dsa.beeter.auth;
+
+import javax.management.relation.Role;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInfo implements Principal
+{
+    private String name;
+    private List<Role> roles = new ArrayList<>();
+
+    public UserInfo() {
+    }
+
+    public UserInfo(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getName()
+    {
+        return null;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
