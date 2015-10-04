91ebdcdff50aae3a22634f775a99f830756d740a
Carolina Rueda <lina_bcn@hotmail.com>
2015-10-04 15:10:52 +0200

    Project skeleton created

---
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
@@ -0,0 +1,10 @@
+package edu.upc.eetac.dsa.beeter;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class BeeterResourceConfig extends ResourceConfig
+{
+    public BeeterResourceConfig() {
+        packages("edu.upc.eetac.dsa.beeter");
+    }
+}
